69fa3dbf635fde807c8fd0d3a408a68cb7256ea7
Shaaran <thisisshaaran@gmail.com>
2020-09-13 16:15:04 +0530

    chessBoard

--- src/App.js
+++ src/App.js
@@ -14,14 +14,81 @@ function App() {
 
   return (
     //JSX Code
-    <div className="App">
-      <Button button_color = {color}/>
-      <Button button_color = {color}/>
-      <Button button_color = {color}/>
-      <Button button_color = {color}/>
+    <div class="chessboard">
+    {/* <!-- 1st --> */}
+    <div class="white">&#9820;</div>
+    <div class="black">&#9822;</div>
+    <div class="white">&#9821;</div>
+    <div class="black">&#9819;</div>
+    <div class="white">&#9818;</div>
+    <div class="black">&#9821;</div>
+    <div class="white">&#9822;</div>
+    <div class="black">&#9820;</div>
+    {/* <!-- 2nd --> */}
+    <div class="black">&#9821;</div>
+    <div class="white">&#9821;</div>
+    <div class="black">&#9821;</div>
+    <div class="white">&#9821;</div>
+    <div class="black">&#9821;</div>
+    <div class="white">&#9821;</div>
+    <div class="black">&#9821;</div>
+    <div class="white">&#9821;</div>
+    {/* <!-- 3th --> */}
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    {/* <!-- 4st --> */}
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    {/* <!-- 5th --> */}
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    {/* <!-- 6th --> */}
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    <div class="black"></div>
+    <div class="white"></div>
+    {/* <!-- 7th --> */}
+    <div class="white">&#9817;</div>
+    <div class="black">&#9817;</div>
+    <div class="white">&#9817;</div>
+    <div class="black">&#9817;</div>
+    <div class="white">&#9817;</div>
+    <div class="black">&#9817;</div>
+    <div class="white">&#9817;</div>
+    <div class="black">&#9817;</div>
 
-      <button onClick={() => setcolor("blue")}>Click to change color</button>
-    </div>
+    {/* <!-- 8th --> */}
+    <div class="black">&#9814;</div>
+    <div class="white">&#9816;</div>
+    <div class="black">&#9815;</div>
+    <div class="white">&#9813;</div>
+    <div class="black">&#9812;</div>
+    <div class="white">&#9815;</div>
+    <div class="black">&#9816;</div>
+    <div class="white">&#9814;</div>
+</div>
   );
 }
 
